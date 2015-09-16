4d3ad4c52d7a50bdfe139717ec0c4875ba8f7a24
Anti-neutrino <myordanov359@gmail.com>
2015-09-16 18:09:18 +0300

    Data Structures

---
+++ DataStructures/Advanced Tree Structures/AATree/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+    public class AATree<TKey,TValue> where TKey:IComparable<TKey>
+    {
+        private class Node
+        {
+            internal int level;
+            internal Node left;
+            internal Node rigth;
+
+            internal TKey key;
+            internal TValue value;
+
+            internal Node()
+            {
+                this.level = 0;
+                this.left = this;
+                this.rigth = this;
+            }
+
+            internal Node(TKey key,TValue value,Node Sentinel)
+            {
+                this.level = 1;
+                this.left = Sentinel;
+                this.rigth = Sentinel;
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private Node root;
+        private Node sentinel;
+        private Node deleted;
+
+        public AATree()
+        {
+            this.root = this.sentinel = new Node();
+            deleted = null;
+        }
+
+        //rotate rigth
+        private void Skew(ref Node node)
+        {
+            if(node.level==node.left.level)
+            {
+                Node left = node.left;
+                node.left = node.rigth;
+                left.rigth = node;
+                node = left;
+            }
+        }
+
+        //rotate left
+        private void Split(ref Node node)
+        {
+            if(node.rigth.rigth.level==node.level)
+            {
+                Node right = node.rigth;
+                node.rigth = right.left;
+                right.left = node;
+                node = right;
+                node.level++;
+            }
+        }
+
+        private bool Insert(ref Node node,TKey key,TValue value)
+        {
+            if(node==sentinel)
+            {
+                node = new Node(key, value, sentinel);
+                return true;
+            }
+
+            int compare = key.CompareTo(node.key);
+            if(compare<0)
+            {
+                if(!Insert(ref node.left,key,value))
+                {
+                    return false;
+                }
+            }
+            else if(compare>0)
+            {
+                if(!Insert(ref node.rigth,key,value))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            Skew(ref node);
+            Split(ref node);
+
+            return true;
+        }
+
+        private bool Delete(ref Node node,TKey key)
+        {
+            if(node==sentinel)
+            {
+                return deleted != null;
+            }
+
+            int compare = key.CompareTo(node.key);
+            if (compare < 0)
+            {
+                if (!Delete(ref node.left, key))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if(compare==0)
+                {
+                    deleted = node;
+                }
+                if(!Delete(ref node.rigth,key))
+                {
+                    return false;
+                }
+            }
+
+            if(deleted!=null)
+            {
+                deleted.key = node.key;
+                deleted.value = node.value;
+                deleted = null;
+                node = node.rigth;
+            }
+            else if(node.left.level<node.level-1||node.rigth.level<node.level-1)
+            {
+                --node.level;
+                if(node.rigth.level>node.level)
+                {
+                    node.rigth.level = node.level;
+                }
+
+                Skew(ref node);
+                Skew(ref node.rigth);
+                Skew(ref node.rigth.rigth);
+                Split(ref node);
+                Split(ref node.rigth);
+            }
+
+            return true;
+        }
+
+        private Node Search(Node node, TKey key)
+        {
+            if (node == sentinel)
+            {
+                return null;
+            }
+
+            int compare = key.CompareTo(node.key);
+            if (compare > 0)
+            {
+                return Search(node.rigth, key);
+            }
+            else if (compare < 0)
+            {
+                return Search(node.left, key);
+            }
+            else
+            {
+                return node;
+            }
+        }
+
+        public bool Add(TKey key,TValue value)
+        {
+            return Insert(ref root, key, value);
+        }
+
+        public bool Remove(TKey key)
+        {
+            return Delete(ref root, key);
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                Node node = Search(root, key);
+                return node == null ? default(TValue) : node.value;
+            }
+            set
+            {
+                Node node = Search(root, key);
+                if(node==null)
+                {
+                    Add(key, value);
+                }
+                else
+                {
+                    node.value = value;
+                }
+            }
+        }
+    }
+}
---
+++ DataStructures/Advanced Tree Structures/IntervalTree/Interval.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntervalTree
+{
+    public enum ContainConstrains
+    {
+        None,
+        IncludeStart,
+        IncludeEnd,
+        IncludStartAndEnd
+    }
+
+   public class Interval<T,D> :IComparable<Interval<T,D>> where D:IComparable<D>
+   {
+       private D start;
+       private D end;
+       private T data;
+
+       public Interval(D start,D end,T data)
+       {
+           this.start = start;
+           this.end = end;
+           this.data = data;
+       }
+
+       public D Start
+       {
+           get { return start; }
+           set { start = value; }
+       }
+
+       public D End
+       {
+           get { return end; }
+           set { end = value; }
+       }
+
+       public T Data
+       {
+           get { return data; }
+           set { data = value; }
+       }
+
+       public bool Contains(D data,ContainConstrains constrains)
+       {
+           bool isContained = false;
+
+           switch(constrains)
+           {
+               case ContainConstrains.None:
+                   isContained = Contains(data);break;
+               case ContainConstrains.IncludeStart:
+                   isContained = ContainsWithStart(data);break;
+               case ContainConstrains.IncludeEnd:
+                   isContained = ContainsWithEnd(data);break;
+               case ContainConstrains.IncludStartAndEnd:
+                   isContained=ContainsWithStartAndEnd(data);break;
+               default: throw new ArgumentException("Invalid operation.");
+           }
+
+           return isContained;
+       }
+
+       private bool ContainsWithStart(D data)
+       {
+           return data.CompareTo(end) < 0 && data.CompareTo(start) >= 0;
+       }
+
+       private bool ContainsWithEnd(D data)
+       {
+           return data.CompareTo(end) <= 0 && data.CompareTo(start) > 0;
+       }
+
+       private bool ContainsWithStartAndEnd(D data)
+       {
+           return data.CompareTo(end) <= 0 && data.CompareTo(start) >= 0;
+       }
+
+       public bool Contains(D time)
+       {
+           return time.CompareTo(end) < 0 && time.CompareTo(start) > 0;
+       }
+
+       public bool Intersects(Interval<T,D> other)
+       {
+           return other.End.CompareTo(this.start) > 0 && other.Start.CompareTo(this.end) < 0;
+       }
+
+       public int CompareTo(Interval<T, D> other)
+       {
+          if(start.CompareTo(other.Start)<0)
+          {
+              return -1;
+          }
+          else if(start.CompareTo(other.Start)>0)
+          {
+              return 1;
+          }
+          else if(end.CompareTo(other.End)<0)
+          {
+              return -1;
+          }
+          else if(end.CompareTo(other.End)>0)
+          {
+              return 1;
+          }
+
+          return 0;
+       }
+
+       public override string ToString()
+       {
+           return string.Format("{0}--{1}", start, end);
+       }
+   }
+}
