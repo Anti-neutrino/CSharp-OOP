bd2d82d275b81eb7aae3fcd4535e50641e352cd6
Anti-neutrino <myordanov359@gmail.com>
2015-10-12 19:09:47 +0300

    Refactorings

--- High-Quality-Code/Exam Preparation/Niki/Cpu.cs
+++ High-Quality-Code/Exam Preparation/Niki/Cpu.cs
@@ -22,16 +22,16 @@
         {
             if (this.numberOfBits == 32)
             {
-                this.SquareNumber32();
+                this.SquareNumber(500);
             }
 
             if (this.numberOfBits == 64)
             {
-                this.SquareNumber64();
+                this.SquareNumber(1000);
             }
         }
 
-        public void SquareNumber32()
+        public void SquareNumber(int maxValue)
         {
             var data = this.ram.LoadValue();
             if (data < 0)
@@ -39,29 +39,6 @@
                 this.videoCard.Draw("Number too low.");
             }
             else if (data > 500)
-            {
-                this.videoCard.Draw("Number too high.");
-            }
-            else
-            {
-                int value = 0;
-                for (int i = 0; i < data; i++)
-                {
-                    value += data;
-                }
-
-                this.videoCard.Draw(string.Format("Square of {0} is {1}.", data, value));
-            }
-        }
-
-        public void SquareNumber64()
-        {
-            var data = this.ram.LoadValue();
-            if (data < 0)
-            {
-                this.videoCard.Draw("Number too low.");
-            }
-            else if (data > 1000)
             {
                 this.videoCard.Draw("Number too high.");
             }
